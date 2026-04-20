e8a3607b6552f5de1c029a5322bea416d5fe6825
fxf-bdgg <julanguinto@gmail.com>
2026-04-21 05:47:15 +0800

    Update auth-guard.js

--- assets/js/auth-guard.js
+++ assets/js/auth-guard.js
@@ -1,18 +1,18 @@
 (function () {
   const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyDjaMdeh0Cgx00hzDyZOi54fDkR81wnxJU",
+    authDomain: "bdgg-database.firebaseapp.com",
+    projectId: "bdgg-database",
+    storageBucket: "bdgg-database.appspot.com",
+    messagingSenderId: "43574975434",
+    appId: "1:43574975434:web:4c79e581267fdfcc6ccd33"
   };
 
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
   }
 
-  const auth = firebase.auth();
-
-  auth.onAuthStateChanged((user) => {
+  firebase.auth().onAuthStateChanged((user) => {
     const currentPage = window.location.pathname.split("/").pop() || "index.html";
     const publicPages = ["login.html"];
 
@@ -22,7 +22,7 @@
     }
 
     if (user && !user.emailVerified && !publicPages.includes(currentPage)) {
-      auth.signOut().then(() => {
+      firebase.auth().signOut().then(() => {
         window.location.replace("login.html");
       });
       return;
