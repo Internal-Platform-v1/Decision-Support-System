3cd5faeff5c0b197252601e0b10631d92fad1895
Internal Platform <julanguinto@gmail.com>
2026-08-05 02:04:34 +0800

    Update theme-loader.js

--- assets/js/theme-loader.js
+++ assets/js/theme-loader.js
@@ -1,7 +1,13 @@
 (function () {
   const THEME_STORAGE_KEY = "dssGuideTheme";
   const DEFAULT_THEME = "theme-fedex";
-  const ALLOWED_THEMES = ["theme-fedex", "theme-atlas", "theme-basic", "theme-simple"];
+  const ALLOWED_THEMES = [
+  "theme-fedex",
+  "theme-atlas",
+  "theme-basic",
+  "theme-simple",
+  "theme-modern"
+];
 
   function getSavedTheme() {
     const savedTheme = localStorage.getItem(THEME_STORAGE_KEY);
